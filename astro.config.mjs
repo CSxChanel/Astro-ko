cdb02cb08e8bab9f16bd8d72de44c6098b68d61e
CSxChanel <csudrajat80@gmail.com>
2024-09-09 09:00:15 +0700

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel/serverless";
 
-import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-  integrations: [tailwind(), react()]
+    output: "server",
+    adapter: vercel(),
+    integrations: [tailwind(), react()]
 });
 // astro.config.mjs
